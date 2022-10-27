72276898ca67eafaf0dacabb8b800f6e9f80bb75
yanist1 <yanistiani1@gmail.com>
2022-10-27 11:13:17 +0700

    adjust data

--- test/data/wheater.data.js
+++ test/data/wheater.data.js
@@ -1,11 +1,9 @@
 export const weather_params = {
     "lat": "40.730610",
-    "lon": "-73.935242",
-    "key": "6ec8ab1f21464ae4ab53c6b03776dca7"
+    "lon": "-73.935242"
 }
 
 export const forecast_params = {
     "postal_code": "28546",
-    "hours": "3",
-    "key": "6ec8ab1f21464ae4ab53c6b03776dca7"
+    "hours": "3"
 }
